0311971af6b697bcb46dbf12cc9d84ebe3334669
msol9 <msol9>
2025-11-07 18:55:01 -0600

    //Creacion del menu

--- main.java
+++ main.java
@@ -1,1 +1,30 @@
 //Modelos de inventarios
+
+
+import java.util.Scanner;
+
+public class main{
+    public static void main(String []args){
+        Scanner sc= new Scanner (System.in);
+        byte opcion=0;
+        do{
+            System.out.println("\n\n\t Modelos de inventarios (EOQ)");
+            System.out.println("1.Modelo EOQ Clasico");
+            System.out.println("2.Modelo EOQ con descuento");
+            System.out.println("Salir");
+            System.out.println("Opcion");
+            opcion=sc.nextByte();
+
+        switch (opcion){
+            case 1-> {
+
+            }
+            case 2-> {
+
+            }
+        }    
+        }while(opcion!=3);
+    }
+
+    }
+}
